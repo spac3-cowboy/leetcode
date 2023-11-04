24a38c763565b53d998a00ff549d9d55df63c78a
spac3-cowboy <116919147+spac3-cowboy@users.noreply.github.com>
2023-11-04 17:20:51 +0600

    2318

---
+++ 2318 Number of Distinct Roll Sequences.py
@@ -0,0 +1,17 @@
+import math
+
+MOD = 10**9 + 7
+
+def numRolls(n):
+    dp = [[[0]*7 for _ in range(7)] for _ in range(n+1)]
+    
+    for i in range(1, 7):
+        dp[1][0][i] = 1
+    for i in range(2, n+1):
+        for j in range(7):
+            for k in range(1, 7):
+                for l in range(1, 7):
+                    if math.gcd(k, l) == 1 and j != l:
+                        dp[i][k][l] = (dp[i][k][l] + dp[i-1][j][k]) % MOD
+    
+    return sum(sum(dp[n][j]) for j in range(7)) % MOD
