a08ca3b3d5b07614e938e96a092784f3f5948a97
spac3-cowboy <116919147+spac3-cowboy@users.noreply.github.com>
2023-11-17 13:10:07 +0600

    90

--- 90.py
+++ 90.py
@@ -1,13 +1,16 @@
 from typing import List
 
+
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         def dfs(start, path):
-            res.append(path)
+            res.append(list(path))
             for i in range(start, len(nums)):
                 if i > start and nums[i] == nums[i - 1]:
                     continue
-                dfs(i + 1, path + [nums[i]])
+                path.append(nums[i])
+                dfs(i + 1, path)
+                path.pop()
 
         nums.sort()
         res = []
